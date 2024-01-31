79cf802875b87a1d99def0c0561636e422b9305e
Amany Mohamed <ayabdelaziz@WX-5CG4473BXB>
2024-01-31 10:21:52 +0200

    Add project files.

---
+++ Generate HashPassword/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+using System.Text;
+
+
+string GenerateHashPassword(string pass)
+{
+    byte[] passwordBytes = Encoding.UTF8.GetBytes(pass);
+    byte[] hashBytes = SHA256.Create().ComputeHash(passwordBytes);
+    return Convert.ToBase64String(hashBytes);
+}
+Console.WriteLine("Hello, World!");
+Console.WriteLine("max tries is 10");
+int i = 0;
+int cont = 0;
+while (i < 10)
+{
+ 
+    Console.WriteLine("enter pass ::");
+    string pass = Console.ReadLine();
+
+    string hashPass = GenerateHashPassword(pass);
+    Console.WriteLine("hashed pass ::" + hashPass);
+
+
+    if (i<10)
+    {
+        Console.WriteLine("Do you want to continue ? \n 1)Yes \n 2)No");
+        cont = int.Parse(Console.ReadLine());
+    }
+    if (cont == 1) i++;
+    else break;
+
+
+}
+Console.WriteLine("Thanks :)");
+Console.ReadKey();
+
+
+
+
